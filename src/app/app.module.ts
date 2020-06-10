13e9e7f51eecb84fef44aa188ad3d1a32027ecfc
Eden Fils <ednfils@gmail.com>
2020-06-10 00:18:22 -0400

    setting up pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header/header.component';
-import { FooterComponent } from './shared/footer/footer/footer.component';
-import { MainLayoutComponent } from './shared/main-layout/main-layout/main-layout.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { HomeComponent } from './layouts/home/home.component';
+import { MovieComponent } from './layouts/movie/movie.component';
+import { ScheduleComponent } from './layouts/schedule/schedule.component';
+import { CheckoutComponent } from './layouts/checkout/checkout.component';
+import { ThankYouComponent } from './layouts/thank-you/thank-you.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    HomeComponent,
+    MovieComponent,
+    ScheduleComponent,
+    CheckoutComponent,
+    ThankYouComponent,
   ],
   imports: [
     BrowserModule,
