8b61352428275bb497d9ba585173e2492ba8bc5c
Eden Fils <ednfils@gmail.com>
2020-06-10 00:07:11 -0400

    main-layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header/header.component';
 import { FooterComponent } from './shared/footer/footer/footer.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
