1b39576a974355f3777c8b240c064740107cdb25
Eden Fils <ednfils@gmail.com>
2020-06-15 00:54:40 -0400

    Setting up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ThankYouComponent } from './layouts/thank-you/thank-you.component';
+import { CheckoutComponent } from './layouts/checkout/checkout.component';
+import { ScheduleComponent } from './layouts/schedule/schedule.component';
+import { MovieComponent } from './layouts/movie/movie.component';
+import { HomeComponent } from './layouts/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'movies/:title',
+    component: MovieComponent,
+  },
+
+  {
+    path: 'movies/:title/schedule',
+    component: ScheduleComponent,
+  },
+
+  {
+    path: 'movies/:title/schedule/checkout',
+    component: CheckoutComponent,
+  },
+
+  {
+    path: 'movies/:title/schedule/checkout/thank-you',
+    component: ThankYouComponent,
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
